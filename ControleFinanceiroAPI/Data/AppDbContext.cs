ad6843460095cdefbbf6764cbf502ad89b50a7e0
voitechen <bia.aran@hotmail.com>
2025-05-03 17:44:20 -0300

    Criação do AppDbContext

---
+++ ControleFinanceiroAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ControleFinanceiroAPI.Models;
+
+namespace ControleFinanceiroAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Transacao> Transacoes => Set<Transacao>();
+    public DbSet<Categoria> Categorias => Set<Categoria>();
+}

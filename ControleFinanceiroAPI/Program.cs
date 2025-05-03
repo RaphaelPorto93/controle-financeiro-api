4a3cf1b0f1749ff8aab81994279f6ecd34cf0e2c
RaphaelPorto93 <raphael_porto_93@hotmail.com>
2025-05-03 17:51:43 -0300

    Configuracao inicial da Minimal API com endpoint GET /transacoes

--- ControleFinanceiroAPI/Program.cs
+++ ControleFinanceiroAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ControleFinanceiroAPI.Data;
+using ControleFinanceiroAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=financeiro.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/transacoes", async (AppDbContext db) =>
+    await db.Transacoes.ToListAsync());
 
 app.Run();

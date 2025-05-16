88facfab8813d7915de9465b7b8c794f85d35487
RaphaelPorto93 <raphael_porto_93@hotmail.com>
2025-05-16 09:36:56 -0300

    endpoints Get/{id}, PUT e DELETE de Categoria

--- ControleFinanceiroAPI/Endpoints/CategoriaEndpoints.cs
+++ ControleFinanceiroAPI/Endpoints/CategoriaEndpoints.cs
@@ -16,6 +16,32 @@ public static class CategoriaEndpoints
             db.Categorias.Add(categoria);
             await db.SaveChangesAsync();
             return Results.Created($"/categorias/{categoria.Id}", categoria);
+        
+    app.MapGet("/categorias/{id}", async (int id, AppDbContext db) =>
+        await db.Categorias.FindAsync(id) is Categoria categoria
+            ? Results.Ok(categoria)
+            : Results.NotFound());
+
+    app.MapPut("/categorias/{id}", async (int id, Categoria input, AppDbContext db) =>
+    {
+        var categoria = await db.Categorias.FindAsync(id);
+        if (categoria is null) return Results.NotFound();
+
+        categoria.Nome = input.Nome;
+        await db.SaveChangesAsync();
+        return Results.Ok(categoria);
+    });
+
+    app.MapDelete("/categorias/{id}", async (int id, AppDbContext db) =>
+    {
+        var categoria = await db.Categorias.FindAsync(id);
+        if (categoria is null) return Results.NotFound();
+
+        db.Categorias.Remove(categoria);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    });
+
         });
     }
 }
